56eefe72553b62f48d170bc1b86808613579dd65
Caroline <carolinejepps@gmail.com>
2020-07-21 16:35:21 +0000

    Added teacher & assistance req + data

--- 2_queries_joins/10_cohorts_total_assignments_duration.sql
+++ 2_queries_joins/10_cohorts_total_assignments_duration.sql
@@ -12,5 +12,5 @@ SELECT students.name as student, count(assignment_submissions.*) as total_submis
 FROM assignment_submissions
 JOIN students ON students.id = student_id
 WHERE students.end_date IS NULL
-AND count(assignment_submissions.*) < 100
-GROUP BY students.name;
+GROUP BY students.name
+HAVING count(assignment_submissions.*) < 100;
